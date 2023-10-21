1894dc467837ce20c3c453c1a97f9fb0f9165243
Gabriela Menezes <gabimenezess10@gmail.com>
2023-10-21 18:46:53 -0300

    feat: movie selected page

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,4 +1,4 @@
-import styles from './MovieCard.module.css'
+import './MovieCard.css'
 import {Link} from 'react-router-dom'
 import {FaStar} from 'react-icons/fa'
 
@@ -6,13 +6,13 @@ const imageUrl = import.meta.env.VITE_IMG;
 
 const MovieCard = ({movie, showLink = true}) => {
   return (
-    <div className={styles.movieCard}>
+    <div className="movieCard">
         <img src={imageUrl + movie.poster_path} alt={`Capa do filme ${movie.title}`} />
         <h2>{movie.title}</h2>
         <p>
             <FaStar /> {movie.vote_average}
         </p>
-        {showLink && <Link to={`/movie/${movie.id}`} className={styles.detalhesBtn}>Detalhes</Link>}
+        {showLink && <Link to={`/movie/${movie.id}`} className="detalhesBtn">Detalhes</Link>}
     </div>
   )
 }
